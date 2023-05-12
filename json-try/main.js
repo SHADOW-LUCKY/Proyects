ef652abbb19295979219c77162c7626058cda2c1
DanielCamiloRamosGarciaCAMPUS <danipro456@hotmail.com>
2023-05-12 08:41:09 -0500

    feat: :fire: start from zero

---
+++ json-try/main.js
@@ -0,0 +1,47 @@
+let myForm = document.querySelector("#myForm");
+myForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    let data = Object.fromEntries(new FormData(e.target));
+    opc[e.submitter.dataset.accion](data)    
+})
+
+const opc = {
+    "GET": () => getUserAll(),
+    "PUT": (data) => putUser(data),
+    "DELETE": (data) => deleteUser(data),
+    "SEARCH": (data) => searchUser(data),
+    "POST": (data) => postUser(data)
+}
+
+let config = {
+    headers:new Headers({
+        "Content-Type": "application/json"
+    }), 
+};
+const getUserAll = async()=>{
+    config.method = "GET";
+    let res = await ( await fetch("http://localhost:4000/usuarios",config)).json();
+    console.log(res);
+}
+const postUser = async(data)=>{
+    config.method = "POST";
+    config.body = JSON.stringify(data);
+    let res = await ( await fetch("http://localhost:4000/usuarios",config)).json();
+    console.log(res);
+}
+const putUser = async(data)=>{
+    config.method = "PUT";
+    config.body = JSON.stringify(data);
+    let res = await ( await fetch(`http://localhost:4000/usuarios/${data.id}`,config)).json();
+    console.log(res);
+}
+const deleteUser = async(data)=>{
+    config.method = "DELETE";
+    let res = await ( await fetch(`http://localhost:4000/usuarios/${data.id}`,config)).json();
+    console.log(res);
+}
+const searchUser = async(data)=>{
+    config.method = "GET";
+    let res = await ( await fetch(`http://localhost:4001/usuarios?q=${Object.values(data).join("")}`,config)).json();
+    console.log(res);
+}
