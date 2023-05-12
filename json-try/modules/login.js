be0254780d17256f47d0108061c113415517c773
SHADOW-LUCKY <nicordonezausecha@gmail.com>
2023-05-12 12:28:08 -0500

    feat: :hammer: login done

--- json-try/modules/login.js
+++ json-try/modules/login.js
@@ -1,20 +1,46 @@
 /* REGISTER */
 const register = document.querySelector('#register')
-
+/* login */
+const login = document.querySelector('#userver')
+/* url */
+const url = 'http://localhost:4000'
 let config = {/* methods */
   headers:new Headers({
       "Content-Type": "application/json"
   }), 
 };
+/* post */
 const postUser = async(e)=> {
   e.preventDefault()
   let data = Object.fromEntries(new FormData(e.target))
   config.method = "POST"
   config.body = JSON.stringify(data)
-  let ans = await fetch('http://localhost:4000/usuarios', config)
+ let ans = await fetch(`${url}/usuarios`, config)
   console.log(ans)
 }
+/* verify */
+const verify = async(e)=> {
+e.preventDefault()
+let data = Object.fromEntries(new FormData(e.target))
+let userVer = await fetch(`${url}/usuarios`) 
+let verify = await userVer.json()
+console.log(data)
+if (verify == false){
+  alert('no hay datos')
+}else{
+  let token = false
+  verify.forEach((log) => {
+    if (data.userVer == log.userName && data.passVer == log.password) {
+      token = true
+    }
+  });
+  if (token == true) {
+    window.location.href = 'index.html'
+  }else{
+    alert('usuario o contraseña incorrecta')
+  }
+}
 
-register.addEventListener('submit',(e)=>{
-  postUser(e)
-} )
+}
+login.addEventListener('submit', verify)
+register.addEventListener('submit', postUser)
