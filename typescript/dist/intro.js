310b764a8b7cb315ff8b13efca189905e96c4bb5
nicordonezausecha@gmail.com <SHADOW-LUCKY>
2023-08-14 09:22:40 -0500

    TYPESCRIPT INIT

---
+++ typescript/dist/intro.js
@@ -0,0 +1,26 @@
+"use strict";
+/* tipos de datos Javascript */
+//number
+let quantity = 50;
+console.log(typeof quantity);
+//string
+let nombre = "Nico";
+console.log(typeof nombre);
+//boolean
+let bool = true;
+console.log(typeof bool);
+let isFired = false;
+console.log(typeof isFired);
+//object
+//null
+//undefined
+//function
+/* tipos de datos Typescript */
+//ANY -> avoid it 
+//unknown
+let alo;
+console.log(typeof alo);
+//never
+//arrays
+//enums
+//tuples
---
+++ typescript/src/intro.ts
@@ -0,0 +1,41 @@
+/* tipos de datos Javascript */
+
+//number
+let quantity:number = 50;
+console.log(typeof quantity);
+//string
+let nombre:string = "Nico";
+console.log(typeof nombre);
+//boolean
+let bool:boolean = true;
+console.log(typeof bool);
+
+let isFired = false;
+console.log(typeof isFired);
+//object
+
+//null
+
+//undefined
+
+//function
+
+/* tipos de datos Typescript */
+
+//ANY -> avoid it 
+let anything:any = "anything";
+anything = 1;
+console.log(anything);
+//unknown
+let alo
+console.log(typeof alo);
+
+
+//never
+
+//arrays
+
+//enums
+
+//tuples
+
